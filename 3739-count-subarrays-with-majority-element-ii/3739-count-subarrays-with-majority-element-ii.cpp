5dd36fe165fbc56c9ae0064be549457d66301e3f
Ankush Raj <ankushraj23341@gmail.com>
2026-06-26 17:34:50 +0530

    Time: 9 ms (86.75%), Space: 95.6 MB (80.79%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
     long long countMajoritySubarrays(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<int> pre(2*n+1,0);
-        //if nums[i]==target then +1 otherwise -1 ,therefore pre will vary from -n to +n (total 2n+1)
-        //negative indices aren't allowed therefore n is the origin
-        pre[n]=1;//means prefix sum zero has appeared once(before starting)
-        //pre[6]=4 means prefix sum corrsponding to index 6 has appeared  four times
-        int count=n;//stores current prefix sum+shift
-        //sum=target+others OR sum=prefix[R]-prefix[L-1],if sum>0 then its a majority subarray
-        //therefor prefix[R]>prefix[L-1],for position r we need to find the no. of previous prefix sum that are smaller than prefix[R]
-        long long ans=0,preSum=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                preSum+=pre[count];
-                count++;
+        int n = nums.size();
+        // frequencyOfPrefix[i] = how many times the shifted prefix sum 'i' has occurred
+        vector<int> frequencyOfPrefix(2 * n + 1, 0);
+        // Initially, prefix sum = 0
+        frequencyOfPrefix[n] = 1;
+        // Current shifted prefix sum index
+        int shiftedPrefix = n;
+        // Number of previous prefix sums that are smaller than the current prefix
+        long long smallerPrefixCount = 0;
+        // Final answer
+        long long majoritySubarrays = 0;
+        for (int num : nums) {
+            if (num == target) {
+                // Prefix sum increases by 1
+                // Prefix sums equal to the current value now become smaller
+                smallerPrefixCount += frequencyOfPrefix[shiftedPrefix];
+                // Move to the new prefix sum
+                shiftedPrefix++;
+                // Record this new prefix sum
+                frequencyOfPrefix[shiftedPrefix]++;
+            } else {
+                // Prefix sum decreases by 1
+                // Move to the new prefix sum
+                shiftedPrefix--;
+                // Prefix sums equal to this value are no longer smaller
+                smallerPrefixCount -= frequencyOfPrefix[shiftedPrefix];
+                // Record this new prefix sum
+                frequencyOfPrefix[shiftedPrefix]++;
             }
-            else{
-                count--;
-                preSum-=pre[count];
-            }
-            pre[count]++;
-            ans+=preSum;
+            // Every smaller prefix forms one majority subarray ending here
+            majoritySubarrays += smallerPrefixCount;
         }
-        return ans;
+        return majoritySubarrays;
     }
 };
