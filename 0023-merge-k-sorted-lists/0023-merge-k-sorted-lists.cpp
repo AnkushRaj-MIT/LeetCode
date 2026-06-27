13f2dffcfad4bd1e48b07314dc5da2c65f6aefb6
Ankush Raj <ankushraj23341@gmail.com>
2026-06-28 00:45:46 +0530

    Time: 7 ms (26.83%), Space: 24.2 MB (5.04%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeSort(ListNode* l1,ListNode* l2){
+        ListNode* dummy=new ListNode(0);
+        ListNode* temp=dummy;
+        while(l1!=NULL && l2!=NULL){
+            if(l1->val<=l2->val){
+                temp->next=l1;
+                l1=l1->next;
+                temp=temp->next;
+            }
+            else{
+                temp->next=l2;
+                l2=l2->next;
+                temp=temp->next;
+            }
+        }
+        while(l1!=NULL){
+            temp->next=l1;
+            l1=l1->next;
+            temp=temp->next;
+        }
+        while(l2!=NULL){
+            temp->next=l2;
+            l2=l2->next;
+            temp=temp->next;
+        }
+        return dummy->next;
+    }
+    ListNode* sortList(ListNode* head) {
+       if(head==NULL||head->next==NULL) return head;
+       ListNode* prev=NULL;
+       ListNode* slow=head;
+       ListNode* fast=head;
+       while(fast!=NULL && fast->next!=NULL){
+        prev=slow;
+        slow=slow->next;
+        fast=fast->next->next;
+       }
+       prev->next=NULL;
+       ListNode* l1=sortList(head);
+       ListNode* l2=sortList(slow);
+       return mergeSort(l1,l2);
+    }    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n=lists.size();
+        if(lists.size()==0) return NULL;
+        ListNode* head=NULL;
+        int idx=0;
+        while(idx<n && lists[idx]==NULL) idx++;
+        if(idx==n) return NULL;
+        head=lists[idx];
+        ListNode* tail=head;
+        while(tail!=NULL && tail->next!=NULL){
+            tail=tail->next;
+        }
+        for(int i=idx+1;i<n;i++){
+            if(lists[i]==NULL) continue;
+           tail->next=lists[i];
+           while(tail->next!=NULL){
+            tail=tail->next;
+           }
+        }
+       return sortList(head);
+    }
+};
