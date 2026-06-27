6dd149ed70f28f714a6d7bae28edc9e1621efe02
Ankush Raj <ankushraj23341@gmail.com>
2026-06-28 00:07:51 +0530

    Time: 48 ms (31.66%), Space: 75.8 MB (7.75%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,20 +10,46 @@
  */
 class Solution {
 public:
+    ListNode* mergeSort(ListNode* l1,ListNode* l2){
+        ListNode* dummy=new ListNode(0);
+        ListNode* temp=dummy;
+        while(l1!=NULL && l2!=NULL){
+            if(l1->val<=l2->val){
+                temp->next=l1;
+                l1=l1->next;
+                temp=temp->next;
+            }
+            else{
+                temp->next=l2;
+                l2=l2->next;
+                temp=temp->next;
+            }
+        }
+        while(l1!=NULL){
+            temp->next=l1;
+            l1=l1->next;
+            temp=temp->next;
+        }
+        while(l2!=NULL){
+            temp->next=l2;
+            l2=l2->next;
+            temp=temp->next;
+        }
+        return dummy->next;
+    }
     ListNode* sortList(ListNode* head) {
-       ListNode* temp=head;
-       vector<int> sorted;
-       while(temp!=NULL){
-        sorted.push_back(temp->val);
-        temp=temp->next;
+       if(head==NULL||head->next==NULL) return head;
+       ListNode* prev=NULL;
+       ListNode* slow=head;
+       ListNode* fast=head;
+       while(fast!=NULL && fast->next!=NULL){
+        prev=slow;
+        slow=slow->next;
+        fast=fast->next->next;
        }
-       sort(sorted.begin(),sorted.end());
-       int idx=0;
-       temp=head;
-       while(temp!=NULL){
-        temp->val=sorted[idx++];
-        temp=temp->next;
-       }
-       return head;
+       prev->next=NULL;
+       ListNode* l1=sortList(head);
+       ListNode* l2=sortList(slow);
+       return mergeSort(l1,l2);
     }
 };
