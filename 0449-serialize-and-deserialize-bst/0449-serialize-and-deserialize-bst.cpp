1aeffef2fe8cab01bbdf77d494e6c0e7eb1c7b95
Ankush Raj <ankushraj23341@gmail.com>
2026-07-05 19:10:48 +0530

    Time: 23 ms (55.7%), Space: 30.9 MB (43.6%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.cpp
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.cpp
@@ -9,34 +9,41 @@
  */
 class Codec {
 public:
-    void preorder(TreeNode* & root,string & ans){
-        if(root==NULL){
-            ans += "#,";
-            return ;
-        }
-        ans += to_string(root->val) + ',';
-        preorder(root->left,ans);
-        preorder(root->right,ans);
-    }
     // Encodes a tree to a single string.
-    string serialize(TreeNode* root) {
-       string ans ="";
-       preorder(root,ans);
-       return ans;
+    void preorder(TreeNode* root, string &s){
+        if(!root) return;
+        s+=to_string(root->val)+",";
+        preorder(root->left, s);
+        preorder(root->right, s);
     }
-    TreeNode * create(stringstream & ss){
-        string current = "";
-        if(!getline(ss,current,','))return NULL;
-        if(current == "#")return NULL;
-        TreeNode* root = new TreeNode(stoi(current));
-        root->left = create(ss);
-        root->right = create(ss);
+    string serialize(TreeNode* root) {
+        string s;
+        preorder(root, s);
+        return s;
+    }
+    TreeNode* build(vector<int>& nums, int &i, int low, int high){
+        if(i==nums.size()) return nullptr;
+        if(nums[i]<low || nums[i]>high) return nullptr;
+        TreeNode* root = new TreeNode(nums[i++]);
+        root->left = build(nums, i, low, root->val);
+        root->right = build(nums, i, root->val, high);
         return root;
     }
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        stringstream ss(data);
-        return create(ss);
+        if(data.size()==0) return nullptr;
+        vector<int> nums;
+        string temp="";
+        for(char c: data){
+            if(c==','){
+                nums.push_back(stoi(temp));
+                temp="";
+            }else{
+                temp+=c;
+            }
+        }
+        int i=0;
+        return build(nums, i, INT_MIN, INT_MAX);
     }
 };
 
